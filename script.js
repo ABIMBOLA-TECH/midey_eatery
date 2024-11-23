74b44990ef1885d69f4616457e336c40564bb47c
TAJUDEEN ABIMBOLA <beamberleak.geospace@yahoo.com>
2024-11-23 13:13:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,290 @@
+
+
+
+document.getElementById("scrollup").addEventListener('click',
+  function() {
+      window.scrollTo({
+        top: 0,
+        behavior:'smooth'
+      });
+    });
+
+
+      function showlist1(){
+        const smallchops = document.querySelector('.smallchops');
+        smallchops.style.display = 'flex';
+
+        const cookies = document.querySelector('.cookies');
+        cookies.style.display = 'none';
+
+        const burger = document.querySelector('.burger');
+        burger.style.display = 'none'
+
+        const chickenchips = document.querySelector('.chickenchips');
+        chickenchips.style.display = 'none'
+
+        const juice = document.querySelector('.juice');
+        juice.style.display = 'none'
+
+        const loaf = document.querySelector('.loaf');
+        loaf.style.display = 'none'
+
+        const pastry = document.querySelector('.pastry');
+        pastry.style.display = 'none'
+
+        const food = document.querySelector('.food');
+        food.style.display = 'none'
+
+        const soup = document.querySelector('.soup');
+        soup.style.display = 'none'
+      };
+
+      function showlist2(){
+        const smallchops = document.querySelector('.smallchops');
+        smallchops.style.display = 'none'
+
+        const cookies = document.querySelector('.cookies');
+        cookies.style.display = 'flex'
+
+        const burger = document.querySelector('.burger');
+        burger.style.display = 'none'
+
+        const chickenchips = document.querySelector('.chickenchips');
+        chickenchips.style.display = 'none'
+
+        const juice = document.querySelector('.juice');
+        juice.style.display = 'none'
+
+        const loaf = document.querySelector('.loaf');
+        loaf.style.display = 'none'
+
+        const pastry = document.querySelector('.pastry');
+        pastry.style.display = 'none'
+
+        const food = document.querySelector('.food');
+        food.style.display = 'none'
+
+        const soup = document.querySelector('.soup');
+        soup.style.display = 'none'
+      };
+
+      function showlist3(){
+        const smallchops = document.querySelector('.smallchops');
+        smallchops.style.display = 'none'
+
+        const cookies = document.querySelector('.cookies');
+        cookies.style.display = 'none'
+
+        const burger = document.querySelector('.burger');
+        burger.style.display = 'flex'
+
+        const chickenchips = document.querySelector('.chickenchips');
+        chickenchips.style.display = 'none'
+
+        const juice = document.querySelector('.juice');
+        juice.style.display = 'none'
+
+        const loaf = document.querySelector('.loaf');
+        loaf.style.display = 'none'
+
+        const pastry = document.querySelector('.pastry');
+        pastry.style.display = 'none'
+
+        const food = document.querySelector('.food');
+        food.style.display = 'none'
+
+        const soup = document.querySelector('.soup');
+        soup.style.display = 'none'
+      };
+
+      function showlist4(){
+        const smallchops = document.querySelector('.smallchops');
+        smallchops.style.display = 'none'
+
+        const cookies = document.querySelector('.cookies');
+        cookies.style.display = 'none'
+
+        const burger = document.querySelector('.burger');
+        burger.style.display = 'none'
+
+        const chickenchips = document.querySelector('.chickenchips');
+        chickenchips.style.display = 'flex'
+
+        const juice = document.querySelector('.juice');
+        juice.style.display = 'none'
+
+        const loaf = document.querySelector('.loaf');
+        loaf.style.display = 'none'
+
+        const pastry = document.querySelector('.pastry');
+        pastry.style.display = 'none'
+
+        const food = document.querySelector('.food');
+        food.style.display = 'none'
+
+        const soup = document.querySelector('.soup');
+        soup.style.display = 'none'
+      };
+
+
+      function showlist5(){
+        const smallchops = document.querySelector('.smallchops');
+        smallchops.style.display = 'none'
+
+        const cookies = document.querySelector('.cookies');
+        cookies.style.display = 'none'
+
+        const burger = document.querySelector('.burger');
+        burger.style.display = 'none'
+
+        const chickenchips = document.querySelector('.chickenchips');
+        chickenchips.style.display = 'none'
+
+        const juice = document.querySelector('.juice');
+        juice.style.display = 'flex'
+
+        const loaf = document.querySelector('.loaf');
+        loaf.style.display = 'none'
+
+        const pastry = document.querySelector('.pastry');
+        pastry.style.display = 'none'
+
+        const food = document.querySelector('.food');
+        food.style.display = 'none'
+
+        const soup = document.querySelector('.soup');
+        soup.style.display = 'none'
+      };
+
+      function showlist6(){
+        const smallchops = document.querySelector('.smallchops');
+        smallchops.style.display = 'none'
+
+        const cookies = document.querySelector('.cookies');
+        cookies.style.display = 'none'
+
+        const burger = document.querySelector('.burger');
+        burger.style.display = 'none'
+
+        const chickenchips = document.querySelector('.chickenchips');
+        chickenchips.style.display = 'none'
+
+        const juice = document.querySelector('.juice');
+        juice.style.display = 'none'
+
+        const loaf = document.querySelector('.loaf');
+        loaf.style.display = 'flex'
+
+        const pastry = document.querySelector('.pastry');
+        pastry.style.display = 'none'
+
+        const food = document.querySelector('.food');
+        food.style.display = 'none'
+
+        const soup = document.querySelector('.soup');
+        soup.style.display = 'none'
+      };
+
+
+      function showlist7(){
+        const smallchops = document.querySelector('.smallchops');
+        smallchops.style.display = 'none'
+
+        const cookies = document.querySelector('.cookies');
+        cookies.style.display = 'none'
+
+        const burger = document.querySelector('.burger');
+        burger.style.display = 'none'
+
+        const chickenchips = document.querySelector('.chickenchips');
+        chickenchips.style.display = 'none'
+
+        const juice = document.querySelector('.juice');
+        juice.style.display = 'none'
+
+        const loaf = document.querySelector('.loaf');
+        loaf.style.display = 'none'
+
+        const pastry = document.querySelector('.pastry');
+        pastry.style.display = 'flex'
+
+        const food = document.querySelector('.food');
+        food.style.display = 'none'
+
+        const soup = document.querySelector('.soup');
+        soup.style.display = 'none'
+      };
+
+
+      function showlist8(){
+        const smallchops = document.querySelector('.smallchops');
+        smallchops.style.display = 'none'
+
+        const cookies = document.querySelector('.cookies');
+        cookies.style.display = 'none'
+
+        const burger = document.querySelector('.burger');
+        burger.style.display = 'none'
+
+        const chickenchips = document.querySelector('.chickenchips');
+        chickenchips.style.display = 'none'
+
+        const juice = document.querySelector('.juice');
+        juice.style.display = 'none'
+
+        const loaf = document.querySelector('.loaf');
+        loaf.style.display = 'none'
+
+        const pastry = document.querySelector('.pastry');
+        pastry.style.display = 'none'
+
+        const food = document.querySelector('.food');
+        food.style.display = 'flex'
+
+        const soup = document.querySelector('.soup');
+        soup.style.display = 'none'
+      };
+
+
+      function showlist9(){
+        const smallchops = document.querySelector('.smallchops');
+        smallchops.style.display = 'none'
+
+        const cookies = document.querySelector('.cookies');
+        cookies.style.display = 'none'
+
+        const burger = document.querySelector('.burger');
+        burger.style.display = 'none'
+
+        const chickenchips = document.querySelector('.chickenchips');
+        chickenchips.style.display = 'none'
+
+        const juice = document.querySelector('.juice');
+        juice.style.display = 'none'
+
+        const loaf = document.querySelector('.loaf');
+        loaf.style.display = 'none'
+
+        const pastry = document.querySelector('.pastry');
+        pastry.style.display = 'none'
+
+        const food = document.querySelector('.food');
+        food.style.display = 'none'
+
+        const soup = document.querySelector('.soup');
+        soup.style.display = 'flex'
+      };
+
+    function showmenu(){
+      const sidebar = document.querySelector('.sidebar')
+      sidebar.style.display = 'flex'
+    };
+
+
+    function closemenu(){
+      const sidebar = document.querySelector('.sidebar')
+      sidebar.style.display = 'none'
+    };
+
+
+    
